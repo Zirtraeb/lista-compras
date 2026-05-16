a0a7ccd44f13459e87328b80c33fd971b5218816
BeatrizA <beatriz.savelar@gmail.com>
2026-05-16 15:25:13 -0300

    creating empty message display function

--- index.js
+++ index.js
@@ -50,4 +50,18 @@ inputCheckbox.addEventListener("click", function() {
     itemDaLista.appendChild(itemData); //o elemento que contém a data completa é adicionado ao item da lista, permitindo que a informação de quando o item foi adicionado seja exibida junto com o nome do item.
     listaDeCompras.appendChild(itemDaLista); //o item da lista é adicionado à lista de compras, tornando-o visível na página.
 
+    verificarListaVazia(); //chamando a função para verificar se a lista está vazia ou não, para exibir ou ocultar a mensagem de lista vazia.
+
 });
+
+
+const mensagemListaVazia = document.querySelector(".lista-vazia"); //queryselector retorna o elemento, no caso a classe listaVazia, usando a mesma sintaxe css de . e #
+
+function verificarListaVazia() {
+    const itensDaLista = listaDeCompras.querySelectorAll("li"); //seleciona todos os elementos li dentro da lista de compras
+    if (itensDaLista.length === 0){
+        mensagemListaVazia.style.display = "block";
+        } else {
+        mensagemListaVazia.style.display = "none"; 
+        } //se a quantidade de itens da lista for igual a zero, a mensagem de lista vazia é exibida, caso contrário, ela é ocultada.
+}
